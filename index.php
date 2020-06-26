cb1d6df2d3393a439a3b5b5d1a72d61da4672526
workofme <usergg0253@gmail.com>
2020-06-26 15:08:15 +0700

    the push

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+include "Database.php";
+$database = new Database;
 include "html/header.php";
 include "html/main.php";
 include "html/footer.php";
+$database->insert('users' , array('user' => 'baobao' , 'pass' => '12345' , 'active' => 1));
